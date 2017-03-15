df8f34fd73e8a21aba4e810e1e3aeb3bd17f9829
Caitlin Hines and Nicole Sanders <caitlin+nicole@>
2017-03-15 15:37:22 -0700

    correctly displays letters in the word

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -2,20 +2,26 @@ var User = require('./../js/dino.js').userModule;
 
 var displayWord = function(thisWord) {
   $(".word").text(thisWord);
-  return thisWord;
 };
 
-
+var word;
 $(document).ready(function() {
+
   var currentUser = new User();
-  var word = currentUser.getWord(displayWord);
-  console.log("word " + word);
+  // console.log("word " + word);
   $(".button").click(function(){
     var letter = $("#user-input").val();
     console.log(letter);
     currentUser.checkLetter(letter, word);
-    currentUser.correctList.forEach(function(){
-      $(".correctList").text("<li>" + correctLetter + "<li>");
+    currentUser.correctList.forEach(function(correctLetter){
+      $(".correctList").append(correctLetter);
     });
   });
+
+  $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1').then(function(response) {
+    displayWord(response);
+    word = response[0].toString();
+    console.log(word);
+  });
+
 });
--- js/dino.js
+++ js/dino.js
@@ -3,14 +3,6 @@ var User = function(){
   this.wrongList = [];
 };
 
-User.prototype.getWord = function(displayWord) {
-  $.get('http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1').then(function(response) {
-    displayWord(response);
-    var final = response[0];
-    console.log(final.toString());
-    return final.toString();
-  });
-};
 
 User.prototype.checkLetter = function(letter, word) {
   if (word.includes(letter)) {
